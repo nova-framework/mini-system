e9aa8a820545f705ba0c21720927e37c0e58c3a0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-19 12:09:27 +0300

    Initial implementation

---
+++ src/Foundation/Bootstrap/BootApplication.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Mini\Foundation\Bootstrap;
+
+use Mini\Foundation\Application;
+
+
+class BootApplication
+{
+	/**
+	 * Bootstrap the given application.
+	 *
+	 * @param  \Mini\Foundation\Application  $app
+	 * @return void
+	 */
+	public function bootstrap(Application $app)
+	{
+		$app->registerConfiguredProviders();
+
+		$app->boot();
+	}
+}
