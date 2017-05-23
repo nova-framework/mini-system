75518d8c43d6b08e36adae724ccba7724fef8e74
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-23 11:08:43 +0300

    Add the Facade for the Assets Router

---
+++ src/Support/Facades/Asset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mini\Support\Facades;
+
+use Mini\Support\Facades\Facade;
+
+
+/**
+ * @see \Mini\Routing\Assets\Router
+ */
+class Assets extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'asset.router'; }
+}
