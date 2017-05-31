7b103207b69ddd71da5fce9f4ad0f32f640d83a7
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-05-31 11:23:08 +0300

    Improve the Mailing Service

--- src/Mail/Console/FlushSpoolQueueCommand.php
+++ src/Mail/Console/FlushSpoolQueueCommand.php
@@ -11,47 +11,62 @@ use Swift_SpoolTransport;
 
 class FlushSpoolQueueCommand extends Command
 {
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
-    protected $name = 'spool:flush';
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'spool:flush';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = "Flush the Mailer's Spool queue";
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = "Flush the Mailer's Spool queue";
+
+	/**
+	 * The Swift Transport instance.
+	 *
+	 * @var \Swift_Transport
+	 */
+	protected $transport;
+
+	/**
+	 * The Swift Spool Transport instance.
+	 *
+	 * @var \Swift_SpoolTransport
+	 */
+	protected $spoolTransport;
 
 
-    /**
-     * Create a new Flush Spool Queue Command instance.
-     *
-     * @return void
-     */
-    public function __construct(Swift_Transport $transport, Swift_SpoolTransport $spoolTransport)
-    {
-        parent::__construct();
+	/**
+	 * Create a new Flush Spool Queue Command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(Swift_Transport $transport, Swift_SpoolTransport $spoolTransport)
+	{
+		parent::__construct();
 
-        $this->realTransport = $transport;
+		//
+		$this->transport = $transport;
 
-        $this->spoolTransport = $spoolTransport;
-    }
+		$this->spoolTransport = $spoolTransport;
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return void
-     */
-    public function fire()
-    {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
 		$config = $this->app['config']->get('mailer.spool');
 
 		explode($config);
 
-		// Get the messages from the spool
+		// Get the messages from the spool.
 		$spool = $this->spoolTransport->getSpool();
 
 		// Setup the spool's options.
@@ -60,9 +75,9 @@ class FlushSpoolQueueCommand extends Command
 		$spool->setRetryLimit($retryLimit);
 
 		// Send the messages via the real transport.
-		$result = $spool->flushQueue($this->realTransport);
+		$result = $spool->flushQueue($this->transport);
 
 		$this->info("Sent $result emails!");
-    }
+	}
 
 }
