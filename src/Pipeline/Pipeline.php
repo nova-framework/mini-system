7a69a6a5080b5f79ca16e2d43257222e43c184e7
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-16 04:31:01 +0300

    Improve the Mini\Pipeline\Pipeline

--- src/Pipeline/Pipeline.php
+++ src/Pipeline/Pipeline.php
@@ -5,6 +5,7 @@ namespace Mini\Pipeline;
 use Mini\Container\Container;
 use Mini\Pipeline\Contracts\PipelineInterface;
 
+use BadMethodCallException;
 use Closure;
 
 
@@ -90,13 +91,18 @@ class Pipeline implements PipelineInterface
 	 */
 	protected function call($pipe, $passable, $stack)
 	{
+		// If the pipe is an instance of a Closure, we will just call it directly but
+		// otherwise we'll resolve the pipes out of the container and call it with
+		// the appropriate method and arguments, returning the results back out.
 		if ($pipe instanceof Closure) {
 			return call_user_func($pipe, $passable, $stack);
 		}
 
 		list($name, $parameters) = $this->parsePipeString($pipe);
 
-		$instance = $this->container->make($name);
+		if (! method_exists($instance = $this->container->make($name), 'handle')) {
+			throw new BadMethodCallException("No [handle] method found on class [$name].");
+		}
 
 		return call_user_func_array(array($instance, 'handle'),
 			array_merge(array($passable, $stack), $parameters)
