6f110cb3c966d6244d73f7ee80dbba4d30aac30e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-20 14:48:51 +0300

    Add Http\Middleware\FrameGuard

---
+++ src/Http/Middleware/FrameGuard.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mini\Http\Middleware;
+
+use Closure;
+
+
+class FrameGuard
+{
+	/**
+	 * Handle the given request and get the response.
+	 *
+	 * @param  \Mini\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return \Mini\Http\Response
+	 */
+	public function handle($request, Closure $next)
+	{
+		$response = $next($request);
+
+		$response->headers->set('X-Frame-Options', 'SAMEORIGIN', false);
+
+		return $response;
+	}
+}
